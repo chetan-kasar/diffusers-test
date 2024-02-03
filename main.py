79cf700eefea4ea8ace74c454acf30db1478850a
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2024-02-03 11:51:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 from diffusers import AutoPipelineForText2Image
 import torch
 
 pipeline = AutoPipelineForText2Image.from_pretrained('dataautogpt3/OpenDalleV1.1')
-image = pipeline('Airoplane with red colour passing through clouds ').images[0]
-print(image)
+
+@app.route('/')
+def hello_world():
+    image = pipeline('Airoplane with red colour passing through clouds ').images[0]
+    print(image)
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
