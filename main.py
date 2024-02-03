a378cf98e19d90ba19d5aee01682d1fa443cbd00
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2024-02-03 16:10:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Flask
+import torch
+from diffusers import LCMScheduler, AutoPipelineForText2Image
+
+model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+adapter_id = "latent-consistency/lcm-lora-sdxl"
+
+pipe = AutoPipelineForText2Image.from_pretrained(model_id, torch_dtype=torch.float16, variant="fp16")
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+pipe.to("cuda")
+
+pipe.load_lora_weights(adapter_id)
+pipe.fuse_lora()
 
 app = Flask(__name__)
 
-from diffusers import AutoPipelineForText2Image
-import torch
 
-pipeline = AutoPipelineForText2Image.from_pretrained('dataautogpt3/OpenDalleV1.1')
 
 @app.route('/hello')
 def hello_world():
-    image = pipeline('Airoplane with red colour passing through clouds').images[0]
+    prompt = "realistic boy"
+    image = pipe(prompt=prompt, num_inference_steps=6, guidance_scale=0).images[0]
     print(image)
     return 'Hello, World!'
     
