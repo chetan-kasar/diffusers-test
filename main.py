7494203cdf8f57698a8b27351f80a76cf1f9b81d
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2024-02-03 12:02:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from diffusers import AutoPipelineForText2Image
-import torch
+# from diffusers import AutoPipelineForText2Image
+# import torch
 
-pipeline = AutoPipelineForText2Image.from_pretrained('dataautogpt3/OpenDalleV1.1')
+# pipeline = AutoPipelineForText2Image.from_pretrained('dataautogpt3/OpenDalleV1.1')
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
-    image = pipeline('Airoplane with red colour passing through clouds ').images[0]
-    print(image)
+    # image = pipeline('Airoplane with red colour passing through clouds ').images[0]
+    # print(image)
     return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run()
+    
+@app.route("/")
+def index():
+    return "Index page !!!!!";
+    
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
