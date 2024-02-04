ef9cab37ca491eec837f896666d92b5948688758
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2024-02-04 11:25:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from flask import Flask
 import warnings
-
+from flask_cors import CORS
 from diffusers import DiffusionPipeline
 
 pipe = DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7")
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/hello')
-def hello_world():
-    prompt = "Real boy"
-    images = pipe(prompt=prompt, num_inference_steps=20, guidance_scale=0).images[0]
-    return 'Hello, World!'
+@app.route("/home", methods=["POST"])
+def home():
+    data = request.get_json()
+    prompt = data.get('prompt')
+    print(prompt)'
+    image = pipe(prompt=prompt, num_inference_steps=20, guidance_scale=0).images[0]
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
     
 @app.route("/")
 def index():
